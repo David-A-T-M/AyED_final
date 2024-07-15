6d80f216cd5bb5dfb45a6669af8892c1e5efc25c
David Trujillo Medina <d.trujillo@unc.edu.ar>
2024-07-15 13:34:05 -0300

    AdjNode is now <T> deleted AdjNode.cpp

--- include/AdjNode.hpp
+++ include/AdjNode.hpp
@@ -1,18 +1,103 @@
-//
-// Created by David on 11/07/2024.
-//
+/*
+ * Adjacency Node class to be used in a linked list, stack or queue;
+ * it contains a pointer to data, an int value and a pointer to the next node
+ */
 #pragma once
 
-#include "Node.hpp"
+#include <string>
 
-class Router;       // Forward declaration
+using namespace std;
 
-class AdjNode : public Node<Router> {     // Inherits from Node
+template <typename T>
+class AdjNode {
     private:
-        int weight = 0;
+        T* data;
+        AdjNode* next;
+        int val = 0;
     public:
-        explicit AdjNode(Router*);
-        AdjNode(Router*, int);
-        void setWeight(int);
-        int getWeight() const;
+        using valType = T;
+
+        AdjNode();                              // Default Constructor
+        explicit AdjNode(T*);                   // Constructor with data
+        AdjNode(T*, int);                       // Constructor with data and value
+        AdjNode(T*, AdjNode*);                  // Constructor with data and next
+        AdjNode(T*, AdjNode*, int);             // Constructor with data, next and value
+        ~AdjNode();                             // Default Destructor
+        void setData(T*);                       // Setter for data
+        void setNext(AdjNode*);                 // Setter for next
+        void setVal(int);                       // Setter for value
+        T* getData() const;                     // Getter for data
+        AdjNode<T>* getNext() const;            // Getter for next
+        int getVal() const;                     // Getter for value
+        bool hasNext();                         // Returns true if next points to another node
+        bool operator==(const AdjNode<T>&) const;  // Overloaded == operator
+        string toString() const;                // Returns a string representation of the data
 };
+
+template <typename T>
+AdjNode<T>::AdjNode()
+        : data(nullptr), next(nullptr) {}
+
+template <typename T>
+AdjNode<T>::AdjNode(T* data)
+        : data(data), next(nullptr) {}
+
+template <typename T>
+AdjNode<T>::AdjNode(T* data, int a)
+        : data(data), val(a) {}
+
+template <typename T>
+AdjNode<T>::AdjNode(T* data, AdjNode* next)
+        : data(data), next(next) {}
+
+template <typename T>
+AdjNode<T>::AdjNode(T* data, AdjNode* next, int a)
+        : data(data), next(next), val(a) {}
+
+template <typename T>
+AdjNode<T>::~AdjNode() = default;
+
+template <typename T>
+void AdjNode<T>::setData(T* newData) {
+    this->data = newData;
+}
+
+template <typename T>
+void AdjNode<T>::setNext(AdjNode* newNext) {
+    this->next = newNext;
+}
+
+template <typename T>
+void AdjNode<T>::setVal(int newA) {
+    this->val = newA;
+}
+
+template<typename T>
+T *AdjNode<T>::getData() const {
+    return data;
+}
+
+template <typename T>
+AdjNode<T>* AdjNode<T>::getNext() const {
+    return next;
+}
+
+template <typename T>
+int AdjNode<T>::getVal() const {
+    return this->val;
+}
+
+template <typename T>
+bool AdjNode<T>::hasNext() {
+    return next != nullptr;
+}
+
+template<typename T>
+bool AdjNode<T>::operator==(const AdjNode<T>& node) const {
+    return *data == *node.getData();
+}
+
+template<typename T>
+string AdjNode<T>::toString() const {
+    return data->toString();
+}
--- tst/AdjNodeTest.cpp
+++ tst/AdjNodeTest.cpp
@@ -3,43 +3,67 @@
 //
 #include <gtest/gtest.h>
 #include "AdjNode.hpp"
-#include "Router.hpp"
+#include "Packet.hpp"
 
 class AdjacencyNodeTest : public ::testing::Test {
 protected:
-    Router *router0{}, *router1{};
-    AdjNode *node0{}, *node1{};
+    IPAddress terminalIP{170, 0b11001100};
+    Packet *packet0{}, *packet1{}, *packet2{};
+    AdjNode<Packet> *node0{}, *node1{}, *node2{}, *node3{};
     void SetUp() override {
-        router0 = new Router();
-        router1 = new Router();
-        node0 = new AdjNode(router0);
-        node1 = new AdjNode(router1, 10);
+        packet0 = new Packet(1, 1, 0, terminalIP);
+        packet1 = new Packet(1, 2, 0, terminalIP);
+        node0 = new AdjNode<Packet>(packet0);
+        node1 = new AdjNode<Packet>(packet0, 10);
+        node2 = new AdjNode<Packet>(packet0, node1);
+        node3 = new AdjNode<Packet>(packet0, node2, 20);
     }
     void TearDown() override {
         delete node0;
         delete node1;
+        delete node2;
+        delete node3;
         node0 = nullptr;
         node1 = nullptr;
-        delete router0;
-        delete router1;
-        router0 = nullptr;
-        router1 = nullptr;
+        node2 = nullptr;
+        node3 = nullptr;
+        delete packet0;
+        delete packet1;
+        packet0 = nullptr;
+        packet1 = nullptr;
     }
 };
 
 
 // Constructor and getter tests
-TEST_F(AdjacencyNodeTest, Constructor) {
-    EXPECT_EQ(node0->getData(), router0);
-    EXPECT_EQ(node1->getData(), router1);
-    EXPECT_EQ(node0->getWeight(), 0);
-    EXPECT_EQ(node1->getWeight(), 10);
+TEST_F(AdjacencyNodeTest, DataConstructor) {
+    EXPECT_EQ(node0->getData(), packet0);
+    EXPECT_EQ(node0->getVal(), 0);
+}
+
+TEST_F(AdjacencyNodeTest, DataValConstructor) {
+    EXPECT_EQ(node1->getData(), packet0);
+    EXPECT_EQ(node1->getVal(), 10);
+}
+
+TEST_F(AdjacencyNodeTest, DataNextConstructor) {
+    EXPECT_EQ(node2->getData(), packet0);
+    EXPECT_EQ(node2->getVal(), 0);
+    EXPECT_EQ(node2->getNext(), node1);
+}
+
+TEST_F(AdjacencyNodeTest, DataNextValConstructor) {
+    EXPECT_EQ(node3->getData(), packet0);
+    EXPECT_EQ(node3->getVal(), 20);
+    EXPECT_EQ(node3->getNext(), node2);
 }
 
 // Setter tests
 TEST_F(AdjacencyNodeTest, SetWeight) {
-    node0->setWeight(5);
-    node1->setWeight(15);
-    EXPECT_EQ(node0->getWeight(), 5);
-    EXPECT_EQ(node1->getWeight(), 15);
+    node3->setVal(5);
+    node3->setData(packet1);
+    node3->setNext(node0);
+    EXPECT_EQ(node3->getVal(), 5);
+    EXPECT_EQ(node3->getData(), packet1);
+    EXPECT_EQ(node3->getNext(), node0);
 }
